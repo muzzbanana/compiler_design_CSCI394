dc23c7c238acdbe17441f70de41145eb63f63758
broomweed <milesabaker@gmail.com>
2018-03-22 15:59:37 -0700

    add a bunch of semantics tests

--- tiger_compiler/test_tiger_semantic.cc
+++ tiger_compiler/test_tiger_semantic.cc
@@ -13,14 +13,82 @@ int yyparse(ASTNode::ASTptr *out);
 extern FILE *yyin;
 extern int yylineno;
 
-TEST_CASE("nested if-else", "[basic-parsing]") {
-    FILE *myfile = fopen("test/nestedifelse.tig", "r");
+TEST_CASE("fail on bad record type", "[semantic-check]") {
+    FILE *myfile = fopen("test_semantic/bad_record_type.tig", "r");
     yyin = myfile;
     yylineno = 1;
     ASTNode::ASTptr output = NULL;
     yyparse(&output);
 
-    REQUIRE(output != NULL);
-    REQUIRE(output->eval() == 7);
+    delete output;
+}
+
+TEST_CASE("fail on expression type mismatch", "[semantic-check]") {
+    FILE *myfile = fopen("test_semantic/expr_type_mismatch.tig", "r");
+    yyin = myfile;
+    yylineno = 1;
+    ASTNode::ASTptr output = NULL;
+    yyparse(&output);
+
+    delete output;
+}
+
+TEST_CASE("fail on incorrect nesting", "[semantic-check]") {
+    FILE *myfile = fopen("test_semantic/incorrect_nesting.tig", "r");
+    yyin = myfile;
+    yylineno = 1;
+    ASTNode::ASTptr output = NULL;
+    yyparse(&output);
+
+    delete output;
+}
+
+TEST_CASE("fail on mutually recursive function type mismatch", "[semantic-check]") {
+    FILE *myfile = fopen("test_semantic/mutual_recursive_mismatch.tig", "r");
+    yyin = myfile;
+    yylineno = 1;
+    ASTNode::ASTptr output = NULL;
+    yyparse(&output);
+
+    delete output;
+}
+
+TEST_CASE("fail on recursive argument type mismatch", "[semantic-check]") {
+    FILE *myfile = fopen("test_semantic/recursive_arg_mismatch.tig", "r");
+    yyin = myfile;
+    yylineno = 1;
+    ASTNode::ASTptr output = NULL;
+    yyparse(&output);
+
+    delete output;
+}
+
+TEST_CASE("fail on recursive return type mismatch", "[semantic-check]") {
+    FILE *myfile = fopen("test_semantic/recursive_func_mismatch.tig", "r");
+    yyin = myfile;
+    yylineno = 1;
+    ASTNode::ASTptr output = NULL;
+    yyparse(&output);
+
+    delete output;
+}
+
+TEST_CASE("fail on repeated definition of var", "[semantic-check]") {
+    FILE *myfile = fopen("test_semantic/repeat_definition.tig", "r");
+    yyin = myfile;
+    yylineno = 1;
+    ASTNode::ASTptr output = NULL;
+    yyparse(&output);
+
+    delete output;
+}
+
+TEST_CASE("fail on use of undefined identifier", "[semantic-check]") {
+    FILE *myfile = fopen("test_semantic/undefined_identifier.tig", "r");
+    yyin = myfile;
+    yylineno = 1;
+    ASTNode::ASTptr output = NULL;
+    yyparse(&output);
+
     delete output;
 }
