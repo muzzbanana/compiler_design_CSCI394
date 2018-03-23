c2f8b48167580f2a20a6882c4cf6497b140ced14
broomweed <milesabaker@gmail.com>
2018-03-22 17:13:46 -0700

    symbol table stubs

--- tiger_compiler/semantic.hh
+++ tiger_compiler/semantic.hh
@@ -1,7 +1,28 @@
+#include <map>
 #include "ast.hh"
 
 using namespace tiger;
 
-int semantic_checks(ASTNode::ASTptr ast) {
-    return 0;
-}
+class Type {
+    public:
+        Type(Type& other);
+};
+
+class SymbolTable {
+    public:
+        SymbolTable();
+        SymbolTable(SymbolTable *parent);
+
+        ~SymbolTable();
+
+        // Create a symbol table that's the same as this one but with another entry
+        void insert(std::string name, Type *t);
+
+        Type *lookup(std::string name);
+
+    private:
+        std::map<std::string, Type*> symbols_;
+        SymbolTable *parent_;
+};
+
+int semantic_checks(ASTNode::ASTptr ast);
