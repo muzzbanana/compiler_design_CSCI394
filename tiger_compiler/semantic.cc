7c443cf6afbcb812bdfa39136b246adc883ff757
broomweed <milesabaker@gmail.com>
2018-03-22 17:13:58 -0700

    forgot to add file

---
+++ tiger_compiler/semantic.cc
@@ -0,0 +1,24 @@
+#include "semantic.hh"
+
+Type::Type(Type& other) {
+}
+
+SymbolTable::SymbolTable() : parent_(NULL) {
+}
+
+SymbolTable::SymbolTable(SymbolTable *parent) : parent_(parent) {
+}
+
+SymbolTable::~SymbolTable() {
+}
+
+void SymbolTable::insert(std::string name, Type *t) {
+    symbols_.insert(std::make_pair(name, t));
+}
+
+Type *SymbolTable::lookup(std::string name) {
+}
+
+int semantic_checks(ASTNode::ASTptr ast) {
+    return 0;
+}
