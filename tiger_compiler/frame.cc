036b12023e611a4047a9483bc071bd74e162a5f2
emmettra <emmettra@gmail.com>
2018-04-07 18:18:31 -0700

    fraaaaame

---
+++ tiger_compiler/frame.cc
@@ -0,0 +1,84 @@
+#include "frame.hh"
+#include <deque>
+#include <vector>
+#include <utility>
+#include <string>
+#pragma <once>
+
+frame::frame()
+	:fp(0),sp(0),tempmap(),argsmap(),localsmap(),stack(),temp1addr()
+	{
+	}
+
+void frame::pushframe(std::vector<std::pair<std::string,int>> arguments_passed, std::vector<std::pair<std::string,int>> local_variables){
+	//pushframe(vector arguments_passed, vector local_variables)
+
+	//assumed vector of pairs in order (name,value)
+	//	-> lastsp=self.sp
+	int lastsp = sp;
+	//	-> push new *map, push arguments in order, adding them to argsmap
+	argsmap.push_back(std::vector<std::pair<std::string,int>>);
+	localsmap.push_back(std::vector<std::pair<std::string,int>>);
+	tempmap.push_back(std::vector<std::pair<std::string,int>>);
+	int i = 0-arguments_passed.size();
+	for (std::pair<std::string,int> arg : argsmap) {
+		auto argum = std::make_pair(std::get<0>(arg),i);//name and i
+		argsmap.back.insert(argum);
+		stack.push_back(std::get<1>(arg)); //value
+		sp += 1;
+		i+=1;
+	}
+	//	-> push current fp as a value 
+	stack.push_back(fp);
+	sp += 1;
+	fp = sp;
+	//	-> add locals in order to stack and localsmap
+	i = 0;
+	for (std::pair<std::string,int> local : local_variables) {
+		auto loc = std::make_pair(std::get<0>(local),i);//name and i
+		localsmap.back.insert(loc);
+		stack.push_back(std::get<1>(local)); //value
+		sp += 1;
+		i+=1;
+	}
+	//	-> push return address <= lastsp?
+	stack.push_back(lastsp);
+	sp += 1;
+	//	-> push self.temp1addr <= self.sp
+	temp1addr.push_back(sp);
+}
+
+void frame::popframe(){
+	//popframe() 
+	//	-> takes fp and pops everything before on the stack, 
+	//	-> pop *map, pop temp1addr, 
+	//	-> fp = return address (temp1addr-1), 
+	//	-> sp = fp
+
+}
+
+void frame::addtemp(std::string name,int value){
+
+}
+
+int frame::lookuptemp(std::string name){
+	return 0;
+}
+
+int frame::lookupvar(std::string name){
+	return 0;
+}
+
+
+int main() {
+	auto f = frame();
+	std::string str = "var1";
+	auto a = make_pair(str,2);
+	std::string stg = "var2";
+	auto b = make_pair(stg,18);
+	std::vector<std::pair<std::string,int>> vect;
+	vect.insert(a);
+	vect.insert(b);
+	f.pushframe(vect,vect);
+	return 0;
+}
---
+++ tiger_compiler/frame.hh
@@ -0,0 +1,46 @@
+#include <deque>
+#include <vector>
+#include <utility>
+#include <string>
+
+class frame {
+public:
+	frame();
+	~frame()=default;
+	std::deque<int> temp1addr;
+	int fp;
+	int sp;
+	std::deque<std::vector<std::pair<std::string,int> > > tempmap;
+	std::deque<std::vector<std::pair<std::string,int> > > localsmap;
+	std::deque<std::vector<std::pair<std::string,int> > > argsmap;
+	std::deque<int> stack;
+	void pushframe(std::vector<std::pair<std::string,int>> arguments_passed, std::vector<std::pair<std::string,int>> local_variables);
+	void popframe();
+	void addtemp(std::string name,int value);
+	int lookuptemp(std::string name);
+	int lookupvar(std::string name);
+
+
+private:
+	//stack int temp1addr -> temp1addr first temp address
+	//int fp -> frame pointer of current stack
+	//int sp -> easy to keep track of this this way
+	//stack map tempmap -> stack of temp class(: maps temp names to offsets from temp1addr )
+	//stack map localsmap -> stack mapping (same template as tempmap) for locals
+	//stack map argsmap -> u get the drift, may not be needed?
+	//stack int stack -> the actual stack :0 (of ints??)
+	//init() -> returns frame with self.stack empty, self.temp1addr empty, self.fp=0, self.*map empty
+	//pushframe(vector arguments_passed, vector local_variables)
+	//	-> lastsp=self.sp
+	//	-> push new *map, push arguments in order, adding them to argsmap
+	//	-> push current fp as a value 
+	//	-> add locals in order to stack and localsmap
+	//	-> push return address <= lastsp?
+	//	-> push self.temp1addr <= self.sp
+	//popframe() 
+	//	-> takes fp and pops everything before on the stack, 
+	//	-> pop *map, pop temp1addr, 
+	//	-> fp = return address (temp1addr-1), 
+	//	-> sp = fp
+	//lookupvar() looks through symbol table for previous variable scope
+	};
