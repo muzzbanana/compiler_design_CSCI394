a879fae6e8f32391bdc3dd8c5373439c84172c29
Meaza Abate <muzzishka@gmail.com>
2018-04-15 19:52:47 -0700

    array test; merging is annoying

--- tiger_compiler/test_tiger_ir.cc
+++ tiger_compiler/test_tiger_ir.cc
@@ -207,12 +207,14 @@ TEST_CASE("check function declaration", "[ir-conversion]") {
     fclose(myfile);
 }
 
-TEST_CASE("check function declaration", "[ir-conversion]") {
-    FILE *myfile = fopen("test_ir/funcdecl.tig", "r");
+TEST_CASE("check recursive function from test_semantic", "[ir-conversion]") {
+    FILE *myfile = fopen("test_ir/semantic_recursive.tig", "r");
     yyin = myfile;
     ASTNode::ASTptr output = NULL;
     yyparse(&output);
 
+    const int result = semantic_checks(output);
+    REQUIRE(result == 0);
     const ProgramTree *ir = convert_ast(output);
     std::cout << output->toStr() << std::endl;
     std::cout << ir->toStr() << std::endl;
