2a5ffc28f55f8f38748aa4aaa53d8fdf399c4877
broomweed <milesabaker@gmail.com>
2018-04-20 09:32:55 -0700

    remove useless methods

--- tiger_compiler/code_gen.hh
+++ tiger_compiler/code_gen.hh
@@ -21,23 +21,6 @@ public:
     virtual bool isJump(ASMInstruction *instruction) = 0;
 
 protected:
-    void munchStmtTree(const IRTree::StmtTree *stmt);
-    void munchSeqTree(const IRTree::StmtTree *lhs, const IRTree::Stmt *rhs);
-    Temp *munchExprTree(const IRTree::ExprTree *expr);
-
-    virtual void munchMoveTree(const IRTree::ExprTree *dst, const IRTree::ExprTree *src) = 0;
-    virtual void munchLabelTree(const Label *label) = 0;
-    virtual void munchUJumpTree(const Label *label) = 0;
-    virtual void munchCJumpTree(const IRTree::CJumpTree *cj) = 0;
-    virtual void munchArgs(const IRTree::ExpList &exps, TempList *tsrc) = 0;
-
-    virtual Temp *munchMemTree(IRTree::MemTree *m) = 0;
-    virtual Temp *munchBinOpTree(IRTree::BinOpTree *b) = 0;
-    virtual Temp *munchCallTree(IRTree::CallTree *c) = 0;
-    virtual Temp *munchConstTree(IRTree::ConstTree *c) = 0;
-    virtual Temp *munchTempTree(IRTree::TempTree *t) = 0;
-    virtual Temp *munchNameTree(IRTree::NameTree *n) = 0;
-
     void emit(Instruction *instruction);
     InstructionList instruction_list_;
 };
